ffdc2aff09012ffa289d78e9d82f0f06b646a4df
Benjamin Wulff <benjamin.wulff.de@ieee.org>
2018-10-24 21:41:26 +0200

    moved Dockerfile for ex 4 one folder up

--- 01/run.sh
+++ 01/run.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-docker --version
-
-docker --help
-
-docker run --help
-
-docker ps
-
 docker run ubuntu:latest bash -c 'echo Hello world from $(cat /etc/issue) on $(date)!'
 
 docker run debian:latest bash -c 'echo Hello world from $(cat /etc/issue) on $(date)!'
